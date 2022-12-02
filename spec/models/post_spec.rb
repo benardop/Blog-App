a95cc5dcbc3308253072665b0746eb3f1b3e2828
benardop <benard.oc.p@gmail.com>
2022-12-03 01:17:29 +0300

    Add test  validations for post models

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,17 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   second_user = User.create(Name: 'Lilly', Photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
-                            Bio: 'Teacher from Poland.', posts_counter: 0)
+                            Bio: 'Teacher from Poland.')
 
   first_post = Post.create(user: second_user, title: 'Hello', text: 'This is my first post', comments_counter: 2,
                            likes_counter: 3)
 
-  first_comment = Comment.new(post: first_post, user: second_user, text: 'Hi Tom!')
-  second_comment = Comment.new(post: first_post, user: second_user, text: 'Hi Tom!')
-  third_comment = Comment.new(post: first_post, user: second_user, text: 'Hi Tom!')
-  fourth_comment = Comment.new(post: first_post, user: second_user, text: 'Hi Tom!')
-  fifth_comment = Comment.new(post: first_post, user: second_user, text: 'Hi Tom!')
-  sixth_comment = Comment.new(post: first_post, user: second_user, text: 'Hi Tom!')
+  first_comment = Comment.new(post: first_post, user: second_user, text: 'Hi Ben!')
+  second_comment = Comment.new(post: first_post, user: second_user, text: 'Hi Ben!')
+  third_comment = Comment.new(post: first_post, user: second_user, text: 'Hi Ben!')
+  fourth_comment = Comment.new(post: first_post, user: second_user, text: 'Hi Ben!')
+  fifth_comment = Comment.new(post: first_post, user: second_user, text: 'Hi Ben!')
+  sixth_comment = Comment.new(post: first_post, user: second_user, text: 'Hi Ben!')
 
   context 'Write validation tests for Post Model' do
     it 'is not valid without a title' do
@@ -31,7 +31,7 @@ RSpec.describe Post, type: :model do
     end
 
     it 'is not valid if comments_counter is not integer' do
-      first_post.comments_counter = 'Mavericks'
+      first_post.comments_counter = 'Benard'
       expect(first_post).to_not be_valid
     end
 
@@ -41,7 +41,7 @@ RSpec.describe Post, type: :model do
     end
 
     it 'is not valid if likes_counter is not an integer' do
-      first_post.likes_counter = 'Balitaan'
+      first_post.likes_counter = 'Benard'
       expect(first_post).to_not be_valid
     end
 
@@ -49,6 +49,10 @@ RSpec.describe Post, type: :model do
       first_post.likes_counter = -21
       expect(first_post).to_not be_valid
     end
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:author) }
   end
 
   context 'Write unit tests for Post Model Methods' do
