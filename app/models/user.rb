b5acab53c5765f1623cefb9f13581a4376599e91
benardop <benard.oc.p@gmail.com>
2022-12-16 23:24:13 +0300

    Update user model class with new details

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,6 @@ class User < ApplicationRecord
     posts.order(:created_at).limit(3)
   end
 
-  validates :name, presence: true
-  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :Name, presence: true
+  # validates :PostsCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
